24b357050786f17230496be5ebfdaea337e5beac
vikivk <vigneshpandain041@gmai.com>
2024-11-05 15:53:56 +0530

    first commit

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+    production: true,
     url:process.env["NG_APP_BASE_URL"], 
 };
--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['NG_APP_BASE_URL'] || '', // Add fallback if NG_APP_BASE_URL is undefined
+  apiUrl: process.env['NG_APP_BASE_URL'] || '', 
 };
